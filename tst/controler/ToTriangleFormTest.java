e9cb4eba81a423536cdc4fde6ea62e3c9daf14c9
KhudovekovRoman <Nuadire@gmail.com>
2017-04-10 06:06:37 +0500

    create ToTriangleFormTest

--- tst/controler/ToTriangleFormTest.java
+++ tst/controler/ToTriangleFormTest.java
@@ -2,33 +2,65 @@ package controler;
 
 import model.Matrix;
 import model.exception.*;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class ToTriangleFormTest {
-    @Test
-    public void toTriangle() throws MatrixCalculationException {
-        double[][] matrix = new double [2][2];
-        matrix[0][0] = 1;
-        matrix[0][1] = 2.1;
-        matrix[1][0] = 10;
-        matrix[1][1] = 9;
-        Matrix mat = new Matrix(matrix,null);
-        //Тестовый вывод матрицы
-        System.err.println();
-        for(int y = 0; y < matrix.length; y ++) {
-            for (int x = 0; x < matrix[0].length; x++)
-                System.err.print(matrix[y][x] + " | ");
-            System.err.println();
-        }
-        double result = 0;
-        try {
-            result = FindDef.findDef(mat);
-        }
-        catch (NoSolution ex){};
 
-        System.err.println(result);
+    @Test
+    public void toTriangle() throws NoSolution {
+        double[][] testMatrix = new double [][] {{0.01, 3.21, -3.22, 5.31},
+                                                 {2.1, 0.15, 2.46, 1.42},
+                                                 {0.54, -0.12, 0.79, 2.81},
+                                                 {2.31, -0.61, 3.48, -3.28}};
+
+        double[][] expectedMatrix1 = new double [][] {  {1, -0.606, 0.605, 0.002},
+                                                        {0, 3.321, -0.708, 2.097},
+                                                        {0, 2.494, -1.819, 0.535},
+                                                        {0, 1.491, 1.373, 2.316}};
+
+
+        double[][] expectedMatrix2 = new double [][] {  {1, -0.606, 0.605, 0.002},
+                                                        {0, 1, -0.213, 0.631},
+                                                        {0, 0, 1, 0.813},
+                                                        {0, 0, 0, 1}};
+
+//        //Тестовый вывод матрицы
+//        for(int y = 0; y < testMatrix.length; y ++) {
+//            for (int x = 0; x < testMatrix[0].length; x++)
+//                System.err.print(testMatrix[y][x] + " | ");
+//            System.err.println();
+//        }
+
+
+
+
+
+        //Вторая проверка
+        final Double expectedValue = 1.0;
+        final int NUMBER = 4;
+
+        Matrix testMatrixOne = new Matrix(testMatrix,null);
+
+        final double[][] resultMatrix = ToTriangleForm.ToTriangle(testMatrixOne).getSolut();
+
+        if(resultMatrix == null){ fail(" getSolut() Method return: null");}
+
+        Double Value = resultMatrix[0][0];
+        for (int i = 0; i < resultMatrix.length; i++) {
+            Value *= resultMatrix[i][i];
+        }
+
+        Double actualValue = new BigDecimal(Value).setScale(NUMBER, RoundingMode.UP).doubleValue();
+
+        System.err.println(actualValue);
+        assertEquals(expectedValue, actualValue);
+
+
+//
     }
 
 }
